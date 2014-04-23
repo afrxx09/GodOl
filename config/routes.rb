c99f40ddeed6eb7dcb516618c8c60e20ebb5bc98
afrxx09 <afrxx09@student.lnu.se>
2014-04-23 20:19:35 +0200

    Statiska sidor m.m.

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 GodOl::Application.routes.draw do
+  get "static_pages/start"
+  get "static_pages/kontakt"
+  get "static_pages/om"
+  
   resources :brewery_contacts
 
   resources :contact_types
 
   resources :breweries
 
-  get "welcome/index"
   
-  root 'welcome#index'
+  root 'static_pages#start'
 end
