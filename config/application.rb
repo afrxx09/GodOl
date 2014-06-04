38125aa31df80519a1c8999add26113f27ef006c
afrxx09 <afrxx09@student.lnu.se>
2014-06-04 22:40:35 +0200

    heroku assets

--- config/application.rb
+++ config/application.rb
@@ -20,6 +20,5 @@ module GodOl
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
     config.encoding = "utf-8"
-    config.serve_static_assets = true
   end
 end
