a203f3e24494368676b4d159e8358df12abd0cd3
afrxx09 <afrxx09@student.lnu.se>
2014-06-04 21:39:56 +0200

    heroku..

--- db/migrate/20140509202643_update_brewery_contact_record.rb
+++
@@ -1,6 +0,0 @@
-class UpdateBreweryContactRecord < ActiveRecord::Migration
-  def change
-  	add_column :brewery_contacts, :brewery_id, :integer
-  	add_column :brewery_contacts, :contact_type_id, :integer
-  end
-end
